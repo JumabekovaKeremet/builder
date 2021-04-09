991d06dfbcaab057ac85c02c9cdce07ff60146f9
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-04-09 14:38:29 +0600

    chaged IceceramControl

--- src/components/IcecreamBuilder/IcecreamControls/IcecreamControls.js
+++ src/components/IcecreamBuilder/IcecreamControls/IcecreamControls.js
@@ -1,19 +1,35 @@
 import IcecreamControl from "./IcecreamControl/IcecreamControl";
+import Button from "../../UI/Button/Button";
 import classes from "./IcecreamControls.module.css";
 
 
-const IcecreamControls = ({pots}) => {
-    const results =[];
-    for (const pot in pots) {
-        results.push(<IcecreamControl 
-            key={ingredient}
+const IcecreamControls = ({
+  ingredients,
+  addIngredient,
+  removeIngredient,
+  startOrdering
+}) => {
+  const results = [];
+  let total = 0;
+  for (const ingredient in ingredients) {
+    // Add ingredient number to totals number
+    total += ingredients[ingredient];
+    // Render pizza control for this ingredient
+    results.push(<IcecreamControl
+        key={ingredient}
         add={addIngredient}
         remove={removeIngredient}
+        count={ingredients[ingredient]}
         type={ingredient} />)
-    }
-    return ( 
-        <div className={classes.IcecreamControls}>{results}</div>
-     );
+  }
+
+  return (
+    <div className={classes.IcecreamControls}>
+      <strong>Ingredients</strong>
+      {results}
+      <Button disabled={!total} onClick={startOrdering}>Order</Button>
+    </div>
+  );
 }
  
 export default IcecreamControls;
