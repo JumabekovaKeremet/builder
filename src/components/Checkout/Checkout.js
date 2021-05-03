62630837a2e5d6c6c60ad610fef9cff03791af61
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-05-03 14:32:11 +0600

    add checout form

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,14 +1,22 @@
-import CheckoutSummary from "./CheckoutSummary/CheckoutSummary";
+
+import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
+import IcecreamPreview from "../IcecreamBuilder/IcecreamPreview/IcecreamPreview";
+import CheckoutForm from "./CheckoutForm/CheckoutForm";
 
 const Checkout = ({ history }) => {
-    function cancelCallback() {
-      history.replace('/');
-    }
-    return (
-        <div>
-          <CheckoutSummary cancelCallback={cancelCallback} />
-        </div>
-      );
-    }
+  function cancelCallback() {
+const Checkout = ({ history }) => {
 
+  return (
+    <div>
+      <CheckoutSummary cancelCallback={cancelCallback} />
+      <IcecreamPreview ingredients={{
+        chocolate: 5, 
+        strawbarry: 1,
+        bananas: 2,
+      }} price={150} />
+      <CheckoutForm />
+    </div>
+  );
+}
 export default Checkout;
